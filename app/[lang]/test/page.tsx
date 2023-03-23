280c7ebe434919b5f948d3b537189f4ca9efe3ea
joaogarin <joao.garin@gmail.com>
2023-03-23 09:13:46 +0100

    add test page

---
+++ app/[lang]/test/page.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { Locale } from "../../../i18n-config";
+
+export default async function IndexPage({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  return <div>New test</div>;
+}
